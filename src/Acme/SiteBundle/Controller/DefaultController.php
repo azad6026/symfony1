c9397acf910ed4250395a2e9eb20ec0e6a43842c
azad6026 <azad6026@gmail.com>
2013-11-20 13:17:42 +1100

    created new bundle

---
+++ src/Acme/SiteBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Acme\SiteBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('AcmeSiteBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
